5b3830d2752cccfc8b366ab063a8391a54c04630
chinwe.jing <1207511201@qq.com>
2018-08-08 17:14:03 +0800

    增加一个清除所有缓存的artisan命令

---
+++ app/Console/Commands/ClearProjectCache.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ClearProjectCache extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clear:project-cache {clear=all}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '快速清除项目所有缓存';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $clearContent = $this->argument('clear');
+        switch ($clearContent) {
+            case 'route':
+                Artisan::call('route:clear');
+                $this->info('路由缓存清理成功！');
+                break;
+            case 'view':
+                Artisan::call('view:clear');
+                $this->info('视图缓存清理成功！');
+                break;
+            case 'config':
+                Artisan::call('config:clear');
+                $this->info('配置缓存清理成功！');
+                break;
+            case 'cache':
+                Artisan::call('cache:clear');
+                $this->info('缓存内容清理成功！');
+                break;
+            case 'all':
+                Artisan::call('route:clear');
+                Artisan::call('view:clear');
+                Artisan::call('config:clear');
+                Artisan::call('cache:clear');
+                $this->info('项目缓存清理成功！');
+                break;
+        }
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        //
+        Commands\ClearProjectCache::class, //清除项目所有缓存
     ];
 
     /**
