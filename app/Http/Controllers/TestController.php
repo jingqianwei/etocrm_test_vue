f11e666edb8f85bed9f2839d3e86c685d3db2eea
chinwe.jing <1207511201@qq.com>
2018-08-08 16:10:54 +0800

    练习新建provider来实例化不同的

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
-    //public $customServiceInterface = null;
-
     //测试写一个新的provide，然后根据不同的条件来实例化不同的service
-    public function index(CustomServiceInterface $customServiceInstance)
+    public function index(CustomServiceInterface $customServiceInstance, Request $request)
     {
-        echo $customServiceInstance->testServices();
+        //dd($request->input('key'));
+        if (is_numeric($request->input('key'))) { //通过$request获取到传过来的值，已经去过左右空格而且还可以却确定参数的类型
+            echo $customServiceInstance->testServices();
+        } else {
+            echo '是字符串';
+        }
     }
 }
