23a7d5c181f786643ce5e1a0e08660f3029df5b2
chinwe.jing <1207511201@qq.com>
2018-08-08 16:32:11 +0800

    给项目增加打印执行sql的日志功能

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Services\Contracts\CustomServiceInterface;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,7 @@ class TestController extends Controller
     //测试写一个新的provide，然后根据不同的条件来实例化不同的service
     public function index(CustomServiceInterface $customServiceInstance, Request $request)
     {
+        dd(User::all());
         //dd($request->input('key'));
         if (is_numeric($request->input('key'))) { //通过$request获取到传过来的值，已经去过左右空格而且还可以却确定参数的类型
             echo $customServiceInstance->testServices();
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Monolog\Handler\StreamHandler;
+use Illuminate\Support\Facades\DB;
+use Monolog\Logger;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +16,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (config('app.env') === 'local') { //本地开发环境才打印sql日志
+            DB::listen(
+                function ($sql) {
+                    foreach ($sql->bindings as $i => $binding) {
+                        if ($binding instanceof \DateTime) {
+                            $sql->bindings[$i] = $binding->format("'Y-m-d H:i:s'");
+                        } else {
+                            if (is_string($binding)) {
+                                $sql->bindings[$i] = "'$binding'";
+                            }
+                        }
+                    }
+
+                    // Insert bindings into query
+                    $query = str_replace(array('%', '?'), array('%%', '%s'), $sql->sql);
+                    $query = vsprintf($query, $sql->bindings);
+                    $log = new Logger('sql');
+                    $log->pushHandler(new StreamHandler(storage_path('logs' . DIRECTORY_SEPARATOR . 'query' . DIRECTORY_SEPARATOR . date('Y-m-d') . '.log'),Logger::INFO) );
+                    $log->addInfo('执行的sql语句为：' . $query);
+                }
+            );
+        }
     }
 
     /**
