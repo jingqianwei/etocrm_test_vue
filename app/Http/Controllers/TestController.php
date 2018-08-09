6aab581e5a392167a1c8ee26373629eb437812a9
chinwe.jing <1207511201@qq.com>
2018-08-09 17:54:43 +0800

    laravel事件的练习

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Events\MyEvent;
 use App\Services\Contracts\CustomServiceInterface;
 use Illuminate\Http\Request;
 
@@ -11,6 +12,7 @@ class TestController extends Controller
     //测试写一个新的provide，然后根据不同的条件来实例化不同的service
     public function index(CustomServiceInterface $customServiceInstance, Request $request)
     {
+        event(new MyEvent()); //触发事件
         dd(User::all());
         //dd($request->input('key'));
         if (is_numeric($request->input('key'))) { //通过$request获取到传过来的值，已经去过左右空格而且还可以却确定参数的类型
