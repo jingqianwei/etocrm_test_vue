cd342433e749db71b7ca79319d4cb1268d6c7119
chinwe.jing <1207511201@qq.com>
2018-08-13 17:14:46 +0800

    laravel中phpstorm提示插件安装

--- app/Models/Carousel.php
+++ app/Models/Carousel.php
@@ -10,6 +10,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Carousel
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $link
+ * @property string $src
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Carousel whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Carousel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Carousel whereLink($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Carousel whereSrc($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Carousel whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Carousel whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Carousel extends Model
 {
     protected $fillable = [
