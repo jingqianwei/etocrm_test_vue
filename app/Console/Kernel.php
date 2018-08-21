884aeba5ec6550189ba33d24eb856b772d89f89b
chinwe.jing <1207511201@qq.com>
2018-08-21 17:26:00 +0800

    laravel定时备份数据库

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,6 +29,8 @@ class Kernel extends ConsoleKernel
         // 每日0点执行一次，同步redis最后范文时间到数据库
         $schedule->command('chinwe:sync-user-actived-at')->dailyAt('00:00');
 
+        //每周一的 23:00执行计划任务
+        $schedule->command('db:backup')->mondays()->at('23:00');
     }
 
     /**
