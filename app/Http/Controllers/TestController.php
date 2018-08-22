aeecdaa09cbfe47aa31867d82ec6c1d9caa7956f
chinwe.jing <1207511201@qq.com>
2018-08-22 15:09:12 +0800

    完善laravel模型

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,6 +15,7 @@ class TestController extends Controller
     //测试写一个新的provide，然后根据不同的条件来实例化不同的service
     public function index(CustomServiceInterface $customServiceInstance, Request $request)
     {
+        dd($this->base64());
         //dd($this->example(1, 'abc', 3));//报错，参数必须为整型
         dd($this->example(1, 2, 3));//可变参数，针对与要传的参数很多的情况
         $val = 1;
