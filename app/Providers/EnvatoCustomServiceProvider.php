a80f92913ece0f246bb55c8b304e0d40175e9792
chinwe.jing <1207511201@qq.com>
2018-08-24 10:59:33 +0800

    fix Laravel功能

--- app/Providers/EnvatoCustomServiceProvider.php
+++ app/Providers/EnvatoCustomServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use App\Services\Contracts\CustomServiceInterface;
 use Illuminate\Support\ServiceProvider;
-use App\Services\DemoOne;
-use App\Services\DemoTwo;
 
 class EnvatoCustomServiceProvider extends ServiceProvider
 {
@@ -27,16 +25,21 @@ class EnvatoCustomServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(CustomServiceInterface::class, function ($app) {
-            $config = config('test', 'one'); //可以去配置这个config从而实例化不同的service
+            $config = config('test', 'App\Services\DemoOne'); //可以去配置这个config从而实例化不同的service
 
-            switch ($config) {
-                case 'one':
-                    return new DemoOne();
-                case 'two':
-                    return new DemoTwo();
-                default :
-                    throw new \Exception('config non-existent');
-            }
+            /**
+                // 旧的写法，缺点是，如果类型很多，则会一直加代码，弊端很大
+                switch ($config) {
+                    case 'one':
+                        return new DemoOne();
+                    case 'two':
+                        return new DemoTwo();
+                    default :
+                        throw new \Exception('config non-existent');
+            }*/
+
+            // 新的写法,好处是，无论有多少种类型，只要配置好对应的值即可，可以用动态实例化类，来简化工厂模式的代码
+            return new $config(); //这种实例一定要注意命名空间，不然会实例化错误
         });
     }
 }
